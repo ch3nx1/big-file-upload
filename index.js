ad75ab9ad79093e15afa8ea379fb878223b8bf95
ch3nx1 <l1ch3nx1@gmail.com>
2023-06-17 11:17:22 +0800

    feat:serve resolve file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const uploader = require('express-fileupload');
+const { extname, resolve } = require('path');
+const { writeFile, appendFile } = require('fs/promises');
+const { existsSync } = require('fs');
 const app = express();
 const port = 3000;
 
@@ -14,7 +17,30 @@ app.use(uploader());
 //     response.send('hello');
 // });
 
-app.post('/api/upload', (request, response) => {
+app.post('/api/upload', async (request, response) => {
+    const { name, size, type, hash, offset, chunk } = request.body;
+    const { file } = request.files;
+    console.log(name, size, type, hash, offset, chunk);
+    const ext = extname(name);
+    const fileName = resolve(__dirname, `./public${hash}${ext}`);
+
+    if (offset) {
+        if (!existsSync(fileName)) {
+            response.status(400).send({
+                message: '文件不存在'
+            });
+            return;
+        }
+        await appendFile(fileName, file.data);
+        response.send({
+            data: 'appended'
+        });
+        return;
+    }
+    await writeFile(fileName, file.data);
+    response.send({
+        data: 'created'
+    });
 
 });
 app.listen(port, () => {
