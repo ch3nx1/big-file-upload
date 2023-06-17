7e80ebd9d301a4aff582e3765231bb2adbb01341
ch3nx1 <l1ch3nx1@gmail.com>
2023-06-17 09:20:19 +0800

    feat:serve static files by express-static

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const uploader = require('express-fileupload');
 const app = express();
 const port = 3000;
-app.get('/', (request, response) => {
-    response.send('hello');
+
+app.use('/', express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({
+    urlencoded: true
+}));
+app.use(uploader());
+
+// app.get('/', (request, response) => {
+//     response.send('hello');
+// });
+
+app.post('/api/upload', (request, response) => {
+
 });
 app.listen(port, () => {
     console.log('server is running at:' + port);
